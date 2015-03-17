11a927ffed6867a34f431f5a72287a2962ecef0b
James Raspass <jraspass@gmail.com>
2015-03-17 13:59:42 +0000

    refactor charnames to use state

--- lib/WebDriver/Tiny.pm
+++ lib/WebDriver/Tiny.pm
@@ -12,26 +12,44 @@ use JSON::PP ();
 use Time::HiRes;
 use WebDriver::Tiny::Elements;
 
-# From https://w3c.github.io/webdriver/webdriver-spec.html#sendkeys
-my %chars;
-@chars{ qw/
-    WD_NULL         WD_CANCEL      WD_HELP     WD_BACK_SPACE  WD_TAB
-    WD_CLEAR        WD_RETURN      WD_ENTER    WD_SHIFT       WD_CONTROL
-    WD_ALT          WD_PAUSE       WD_ESCAPE   WD_SPACE       WD_PAGE_UP
-    WD_PAGE_DOWN    WD_END         WD_HOME     WD_ARROW_LEFT  WD_ARROW_UP
-    WD_ARROW_RIGHT  WD_ARROW_DOWN  WD_INSERT   WD_DELETE      WD_SEMICOLON
-    WD_EQUALS       WD_NUMPAD0     WD_NUMPAD1  WD_NUMPAD2     WD_NUMPAD3
-    WD_NUMPAD4      WD_NUMPAD5     WD_NUMPAD6  WD_NUMPAD7     WD_NUMPAD8
-    WD_NUMPAD9      WD_MULTIPLY    WD_ADD      WD_SEPARATOR   WD_SUBTRACT
-    WD_DECIMAL      WD_DIVIDE      WD_F1       WD_F2          WD_F3
-    WD_F4           WD_F5          WD_F6       WD_F7          WD_F8
-    WD_F9           WD_F10         WD_F11      WD_F12         WD_META
-    WD_COMMAND      WD_ZENKAKU_HANKAKU
-/ } = ( 0xE000 .. 0xE029, 0xE031 .. 0xE03D, 0xE03D, 0xE040 );
+sub import {
+    # From https://w3c.github.io/webdriver/webdriver-spec.html#sendkeys
+    state $chars = {
+        WD_NULL            => 57344, WD_CANCEL     => 57345,
+        WD_HELP            => 57346, WD_BACK_SPACE => 57347,
+        WD_TAB             => 57348, WD_CLEAR      => 57349,
+        WD_RETURN          => 57350, WD_ENTER      => 57351,
+        WD_SHIFT           => 57352, WD_CONTROL    => 57353,
+        WD_ALT             => 57354, WD_PAUSE      => 57355,
+        WD_ESCAPE          => 57356, WD_SPACE      => 57357,
+        WD_PAGE_UP         => 57358, WD_PAGE_DOWN  => 57359,
+        WD_END             => 57360, WD_HOME       => 57361,
+        WD_ARROW_LEFT      => 57362, WD_ARROW_UP   => 57363,
+        WD_ARROW_RIGHT     => 57364, WD_ARROW_DOWN => 57365,
+        WD_INSERT          => 57366, WD_DELETE     => 57367,
+        WD_SEMICOLON       => 57368, WD_EQUALS     => 57369,
+        WD_NUMPAD0         => 57370, WD_NUMPAD1    => 57371,
+        WD_NUMPAD2         => 57372, WD_NUMPAD3    => 57373,
+        WD_NUMPAD4         => 57374, WD_NUMPAD5    => 57375,
+        WD_NUMPAD6         => 57376, WD_NUMPAD7    => 57377,
+        WD_NUMPAD8         => 57378, WD_NUMPAD9    => 57379,
+        WD_MULTIPLY        => 57380, WD_ADD        => 57381,
+        WD_SEPARATOR       => 57382, WD_SUBTRACT   => 57383,
+        WD_DECIMAL         => 57384, WD_DIVIDE     => 57385,
+        WD_F1              => 57393, WD_F2         => 57394,
+        WD_F3              => 57395, WD_F4         => 57396,
+        WD_F5              => 57397, WD_F6         => 57398,
+        WD_F7              => 57399, WD_F8         => 57400,
+        WD_F9              => 57401, WD_F10        => 57402,
+        WD_F11             => 57403, WD_F12        => 57404,
+        WD_META            => 57405, WD_COMMAND    => 57405,
+        WD_ZENKAKU_HANKAKU => 57408,
+    };
 
-require charnames;
+    require charnames;
 
-sub import { charnames->import( ':alias' => \%chars ) }
+    charnames->import( ':alias' => $chars );
+}
 
 sub new {
     my ( $class, %args ) = @_;
